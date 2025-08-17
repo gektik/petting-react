5ee48f00bfd728eb44f472530873b8f184f6223d
gektik <36208249+gektik@users.noreply.github.com>
2025-08-17 19:57:50 +0300

    Change negative margin to padding-bottom

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -610,7 +610,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingBottom: 100,
-    marginTop: -100,
+    marginTop: 0,
   },
   animatedCard: {
     position: 'absolute',
