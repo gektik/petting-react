9dd4d7f208b08eef34f204f20a9503e383c9f8fd
gektik <36208249+gektik@users.noreply.github.com>
2025-08-18 22:07:09 +0300

    Expo projesini düzelt

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Ensure proper resolver configuration
-config.resolver.assetExts.push('svg', 'png', 'jpg', 'jpeg', 'gif', 'webp');
-config.resolver.sourceExts.push('jsx', 'js', 'ts', 'tsx', 'json');
-
 module.exports = config;
